a1546ca00e88481633700f1c7c73575c6099193b
Lindson França <l1nds0n@hotmail.com>
2021-06-24 17:19:31 -0300

    Serviços checkin

---
+++ services/Services.Checkins.dfm
@@ -0,0 +1,75 @@
+object ServiceCheckin: TServiceCheckin
+  OldCreateOrder = False
+  OnCreate = DataModuleCreate
+  Height = 150
+  Width = 181
+  object mtCadastroCheckin: TFDMemTable
+    FetchOptions.AssignedValues = [evMode]
+    FetchOptions.Mode = fmAll
+    ResourceOptions.AssignedValues = [rvSilentMode]
+    ResourceOptions.SilentMode = True
+    UpdateOptions.AssignedValues = [uvCheckRequired, uvAutoCommitUpdates]
+    UpdateOptions.CheckRequired = False
+    UpdateOptions.AutoCommitUpdates = True
+    Left = 72
+    Top = 16
+    object mtCadastroCheckinid: TIntegerField
+      AutoGenerateValue = arAutoInc
+      FieldName = 'id'
+    end
+    object mtCadastroCheckindataentrada: TDateField
+      FieldName = 'dataentrada'
+    end
+    object mtCadastroCheckindatasaida: TDateField
+      FieldName = 'datasaida'
+    end
+    object mtCadastroCheckinadicionalveiculo: TBooleanField
+      FieldName = 'adicionalveiculo'
+    end
+    object mtCadastroCheckinhospede: TIntegerField
+      FieldName = 'hospede'
+    end
+  end
+  object mtPesquisaCheckin: TFDMemTable
+    FieldDefs = <>
+    IndexDefs = <>
+    FetchOptions.AssignedValues = [evMode]
+    FetchOptions.Mode = fmAll
+    FormatOptions.AssignedValues = [fvMaxBcdPrecision, fvMaxBcdScale]
+    FormatOptions.MaxBcdPrecision = 2147483647
+    FormatOptions.MaxBcdScale = 1073741823
+    ResourceOptions.AssignedValues = [rvPersistent, rvSilentMode]
+    ResourceOptions.Persistent = True
+    ResourceOptions.SilentMode = True
+    UpdateOptions.AssignedValues = [uvCheckRequired, uvAutoCommitUpdates]
+    UpdateOptions.CheckRequired = False
+    UpdateOptions.AutoCommitUpdates = True
+    StoreDefs = True
+    Left = 72
+    Top = 80
+    object mtPesquisaCheckinid: TIntegerField
+      FieldName = 'id'
+    end
+    object mtPesquisaCheckindataentrada: TDateField
+      FieldName = 'dataentrada'
+    end
+    object mtPesquisaCheckindatasaida: TDateField
+      FieldName = 'datasaida'
+    end
+    object mtPesquisaCheckinadicionalveiculo: TBooleanField
+      FieldName = 'adicionalveiculo'
+    end
+    object mtPesquisaCheckinhospede: TIntegerField
+      FieldName = 'hospede'
+    end
+    object mtPesquisaCheckinlkpHospede: TStringField
+      FieldKind = fkLookup
+      FieldName = 'lkpHospede'
+      LookupDataSet = ServiceHospedes.mtPesquisaHospedes
+      LookupKeyFields = 'id'
+      LookupResultField = 'nome'
+      KeyFields = 'hospede'
+      Lookup = True
+    end
+  end
+end
---
+++ services/Services.Checkins.pas
@@ -0,0 +1,111 @@
+unit Services.Checkins;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  FireDAC.Stan.Intf,
+  FireDAC.Stan.Option,
+  FireDAC.Stan.Param,
+  FireDAC.Stan.Error,
+  FireDAC.DatS,
+  FireDAC.Phys.Intf,
+  FireDAC.DApt.Intf,
+  Data.DB,
+  FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client,
+  FireDAC.Stan.StorageBin,
+  DataSet.Serialize,
+  RESTRequest4D,
+  Services.Hospedes;
+
+type
+  TServiceCheckin = class(TDataModule)
+    mtCadastroCheckin: TFDMemTable;
+    mtCadastroCheckinid: TIntegerField;
+    mtCadastroCheckindataentrada: TDateField;
+    mtCadastroCheckindatasaida: TDateField;
+    mtCadastroCheckinadicionalveiculo: TBooleanField;
+    mtCadastroCheckinhospede: TIntegerField;
+    mtPesquisaCheckin: TFDMemTable;
+    mtPesquisaCheckinid: TIntegerField;
+    mtPesquisaCheckindataentrada: TDateField;
+    mtPesquisaCheckindatasaida: TDateField;
+    mtPesquisaCheckinadicionalveiculo: TBooleanField;
+    mtPesquisaCheckinhospede: TIntegerField;
+    mtPesquisaCheckinlkpHospede: TStringField;
+    procedure DataModuleCreate(Sender: TObject);
+    private
+      { Private declarations }
+    public
+      procedure Salvar;
+      procedure Listar;
+      procedure Delete(const AId: string);
+      procedure GetById(const AId: string);
+  end;
+
+const
+  baseURL = 'http://localhost:9000';
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+{ TServiceCheckin }
+
+procedure TServiceCheckin.DataModuleCreate(Sender: TObject);
+begin
+  mtCadastroCheckin.Open;
+  mtPesquisaCheckin.Open;
+end;
+
+procedure TServiceCheckin.Delete(const AId: string);
+var
+  LResponse: IResponse;
+begin
+  LResponse := TRequest.New.baseURL(baseURL).Resource('checkins').ResourceSuffix(AId).Delete;
+  if not(LResponse.StatusCode = 204) then
+    raise Exception.Create(LResponse.JSONValue.GetValue<string>('error'));
+
+end;
+
+procedure TServiceCheckin.GetById(const AId: string);
+var
+  LResponse: IResponse;
+begin
+  mtCadastroCheckin.EmptyDataSet;
+  LResponse := TRequest.New.baseURL(baseURL).Resource('checkins').ResourceSuffix(AId)
+    .DataSetAdapter(mtCadastroCheckin).Get;
+  if not(LResponse.StatusCode = 200) then
+    raise Exception.Create(LResponse.JSONValue.GetValue<string>('error'));
+
+end;
+
+procedure TServiceCheckin.Listar;
+var
+  LResponse: IResponse;
+begin
+  LResponse := TRequest.New.baseURL(baseURL).Resource('checkins').DataSetAdapter(mtPesquisaCheckin).Get;
+  if not(LResponse.StatusCode = 200) then
+    raise Exception.Create(LResponse.JSONValue.GetValue<string>('error'));
+
+end;
+
+procedure TServiceCheckin.Salvar;
+var
+  LRequest: IRequest;
+  LResponse: IResponse;
+begin
+  LRequest := TRequest.New.baseURL(baseURL).Resource('checkins').AddBody(mtCadastroCheckin.ToJSONObject);
+  if (mtCadastroCheckinid.AsInteger > 0) then
+    LResponse := LRequest.ResourceSuffix(mtCadastroCheckinid.AsString).Put
+  else
+    LResponse := LRequest.Post;
+  if not(LResponse.StatusCode in [201, 204]) then
+    raise Exception.Create(LResponse.JSONValue.GetValue<string>('error'));
+  mtCadastroCheckin.EmptyDataSet;
+
+end;
+
+end.
